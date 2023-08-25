6de1f15dd4a90a3728de75642aa40362f2a41313
Hardik <creatinggalaxy@gmail.com>
2023-08-25 14:23:40 +0530

    initial Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonData, setPokemonData] = useState(null);
+
+  const fetchPokemonData = async () => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      
+      const data = await response.json();
+      setPokemonData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PokeDex by Hardik</h1>
+      <input type="text" placeholder="Enter Pokemon Name" value={pokemonName} onChange={(e) => setPokemonName(e.target.value)}/>
+
+      <button onClick={fetchPokemonData}>Search</button>
+
+      {pokemonData && (
+  <div>
+    <h2>{pokemonData.name}</h2>
+    <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+    <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Abilities</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>{pokemonData.name}</td>
+        <td>
+          {pokemonData.types.map((typeData) => (
+            <span key={typeData.slot}>{typeData.type.name} </span>
+          ))}
+        </td>
+        <td>
+  {pokemonData.abilities.map((abilityData, index) => (
+    <span key={abilityData.slot}>
+      {abilityData.ability.name}
+      {index !== pokemonData.abilities.length - 1 && ", "}
+    </span>
+  ))}
+</td>
+
+      </tr>
+    </tbody>
+  </table>
+  </div>
+)}
+
     </div>
   );
 }
